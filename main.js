e5bb16025014f706c77442af4dac87352f724bc1
Aryadolphin <78508957+arya0402@users.noreply.github.com>
2022-10-14 17:31:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
 leftWristScore = 0;
+rightWristScore = 0;
 song1_status = "";
 song2_status = "";
 
@@ -25,14 +26,25 @@ function setup() {
 function draw() {
     image(video, 0, 0, 500, 500);
     song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
     fill("#FF0000");
     stroke("#FF0000");
-    if(leftWristScore > 0.2) {
+    if (leftWristScore > 0.2) {
         circle(leftWristX, leftWristY, 20);
         song2.stop();
-        if(song1_status == "false") {
+        if (song1_status == "false") {
             song1.play();
             document.getElementById("song_name").innerHTML = "Playing Harry Potter Theme Song";
+        }
+
+    }
+
+    if (rightWristScore > 0.2) {
+        circle(rightWristX, rightWristY, 20);
+        song1.stop();
+        if (song2_status == "false") {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Playing Pater Pan";
         }
     }
 
@@ -44,7 +56,7 @@ function modelLoaded() {
 }
 
 function gotPoses(results) {
-    if(results.length > 0) {
+    if (results.length > 0) {
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
